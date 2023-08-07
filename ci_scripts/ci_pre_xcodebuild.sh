768a4c4e4f54c457a6bdcc8157d76938106661d6
Kiwhan Song <kwsong0113@gmail.com>
2023-08-07 15:48:39 +0900

    fix: build error caused by pre xcodebuild script

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-cd $CI_PROJECT_FILE_PATH/TuringMachine
+cd ../TuringMachine
 
 touch Secret.plist
 
-echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>                          ─╯
-<\!DOCTYPE plist PUBLIC \"-//Apple//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\"\>
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
+<!DOCTYPE plist PUBLIC \"-//Apple//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\"\>
 <plist version=\"1.0\">
 <dict/>
 </plist>" >> Secret.plist
