6a2902bf4d8754ab082b431d36d358d2f3de96d0
Kiwhan Song <kwsong0113@gmail.com>
2023-08-07 14:52:26 +0900

    feat: add a pre-xcodebuild script to write Secret.plist

---
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+cd $CI_PROJECT_FILE_PATH/TuringMachine
+
+touch Secret.plist
+
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>                          ─╯
+<\!DOCTYPE plist PUBLIC \"-//Apple//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\"\>
+<plist version=\"1.0\">
+<dict/>
+</plist>" >> Secret.plist
+
+plutil -replace WEBSOCKET_ENDPOINT_URL -string $WEBSOCKET_ENDPOINT_URL Secret.plist
+plutil -replace API_ENDPOINT_URL -string $API_ENDPOINT_URL Secret.plist
+
+exit 0
